d0fe5ee3ccbf021b58f4e469af0960d646f0b452
Zhiwei Yang <2665477495@qq.com>
2023-05-06 11:35:59 +0800

    up

--- btc/tran.py
+++ btc/tran.py
@@ -40,28 +40,55 @@ class TransactionPool:
 
 class Wallet:
     def __init__(self, balance):
-        self.address, self.private_key = self.generate_key_pair()
+        self.private_key, self.public_key = self.generate_keys()
+        self.address = self.get_address(self.public_key)
         self.balance = balance
 
-    def generate_key_pair(self):
-        key_pair = RSA.generate(2048)
-        private_key = PKCS1_v1_5.new(key_pair)
-        public_key = key_pair.publickey()
-        return (public_key.exportKey(), private_key)
+    def generate_keys(self):
+        rsa_key = RSA.generate(1024)
+        private_key = rsa_key.export_key()
+        public_key = rsa_key.publickey().export_key()
+        return private_key, public_key
+
+    def get_address(self, public_key):
+        sha256_hash = SHA256.new(public_key)
+        return sha256_hash.hexdigest()
+
+    def sign_transaction(self, transaction):
+        signer = PKCS1_v1_5.new(RSA.import_key(self.private_key))
+        h = SHA256.new(str(transaction.sender).encode() + str(transaction.recipient).encode() +
+                       str(transaction.amount).encode() + str(transaction.fee).encode())
+        signature = signer.sign(h)
+        return signature
+
+    def verify_transaction(self, transaction):
+        pubkey = RSA.import_key(self.public_key)
+        rsa = PKCS1_v1_5.new(pubkey)
+        h = SHA256.new(str(transaction.sender).encode() + str(transaction.recipient).encode() +
+                       str(transaction.amount).encode() + str(transaction.fee).encode())
+        try:
+            rsa.verify(h, transaction.signature)
+            return True
+        except:
+            return False
 
     def send(self, recipient, amount, fee):
-        if self.balance >= amount + fee:
-            transaction = Transaction(self.address, recipient, amount, fee)
-            transaction.sign(self.private_key)
-            if transaction.is_valid():
-                self.balance -= amount + fee
-                recipient.receive(amount)
-                transaction_pool.add_transaction(transaction)
+        transaction = Transaction(self.address, recipient, amount, fee)
+        if not transaction_rule.is_valid_transaction(transaction):
+            return False
+        if self.balance < amount + fee:
+            return False
+        transaction.signature = self.sign_transaction(transaction)
+        transaction_pool.add_transaction(transaction)
+        self.balance -= amount + fee
+        return True
+
+    def receive(self, transaction):
+        if self.verify_transaction(transaction):
+            if transaction.recipient == self.address:
+                self.balance += transaction.amount
                 return True
         return False
-
-    def receive(self, amount):
-        self.balance += amount
 
 # 创建 5 个钱包对象，分别有不同的初始资产分布
 wallet1 = Wallet(1000)
@@ -78,24 +105,18 @@ economic_rule = EconomicRule([wallet1, wallet2, wallet3, wallet4, wallet5])
 transaction_pool = TransactionPool()
 
 # 进行 100 次交易模拟
-# 进行 100 次交易模拟
 for i in range(100):
     # 选择两个钱包对象进行交易
     wallets = economic_rule.select_wallets(2)
     sender = wallets[0]
     recipient = wallets[1]
-    # 生成交易金额和手续费
-    max_amount = min(sender.balance - transaction_rule.min_fee, recipient.balance)
-    if max_amount <= 0:
-        continue
-    amount = random.randint(1, max_amount)
-    fee = random.uniform(transaction_rule.min_fee, transaction_rule.max_fee)
+    amount = random.randint(1, 100)
+    fee = random.uniform(0.01, 0.1)
     # 发送交易
     if sender.send(recipient.address, amount, fee):
         print("Transaction sent:", sender.balance, recipient.balance)
     else:
         print("Invalid transaction")
-
 
 # 输出每个钱包对象的最终资产余额
 print("Wallet 1 balance:", wallet1.balance)
